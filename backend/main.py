d28811cb444d1d75ee9a711383176686e7406359
cyprich <cypooriginal@gmail.com>
2025-01-19 13:08:29 +0100

    changed react from typescript to javascript

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, jsonify
+from flask_cors import CORS  # cross-origin resource sharing - for frontend
 from typing import Any
 
 fields = (
@@ -19,14 +20,15 @@ fields = (
 
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
+@app.route("/api", methods=["GET"])
 def index():
     return jsonify(message="Hello from backend!")
 
 
-@app.route("/filaments/")
+@app.route("/api/filaments/", methods=["GET"])
 def filaments():
     curs.execute("SELECT * FROM filaments")
     resp = curs.fetchall()
@@ -39,7 +41,7 @@ def filaments():
     return jsonify(filaments=parsed_filaments)
 
 
-@app.route("/filaments/<int:id>/")
+@app.route("/api/filaments/<int:id>/", methods=["GET"])
 def filament(id: int):
     if id > len(fields):
         return {"error": "Filament index out of range"}, 404
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+//import axios from 'axios';
+
+
+function App() {
+    // axios.get("https://localhost:5000/api")
+    //     .then(response => {console.log(response.data)})
+    //     .catch(error => {console.log(error)})
+
+    console.log("Hello")
+
+    return (
+        <>
+            <h1>Filaments</h1>
+        </>
+    );
+}
+
+export default App;
