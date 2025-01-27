ab95307f170888bb3b4e556756805be4939db9ae
cyprich <cyprich6@stud.uniza.sk>
2025-01-27 23:31:25 +0100

    redone labels, with tailwindcss

--- frontend/src/Labels.jsx
+++ frontend/src/Labels.jsx
@@ -1,14 +1,14 @@
 import {useEffect, useState} from "react";
 
-import FilamentInfo from "./FilamentInfo.jsx";
 import config from "./config.json"
 import axios from "axios";
-
-// TODO redo sizes with em units for printing
+import {useNavigate} from "react-router-dom";
 
 function Labels() {
     const IP = config.ip;
     const [filaments, setfilaments] = useState([])
+
+    let navigate = useNavigate()
 
     useEffect(() => {
         axios.get(`http://${IP}:5000/api/filaments/`)
@@ -19,51 +19,54 @@ function Labels() {
     }, [IP]);
 
     return (
-        <div className={"main"}>
-            <h1 className={"no-print"}>Štítky</h1>
-            <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", justifyItems: "center", gap: "32px 0"}}>
-                {filaments.map((item, key) => (
-                    <div key={key} style={{
-                        scale: "0.9",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "48px",
-                        border: "1px solid black",
-                        padding: "32px",
-                        width: "max-content"
-                    }}>
-                        <div className={"label"} style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "1em",
-                        }}>
-                            <div style={{display: "flex", alignItems: "center", gap: "24px", width: "max-content"}}>
-                                <div
-                                    style={{
-                                        background: `linear-gradient(135deg, ${item.color_hex}, ${item.color_second_hex || item.color_hex})`,
-                                        width: "72px",
-                                        height: "72px",
-                                        borderRadius: "50%",
-                                    }}
-                                />
-                                <div style={{display: "flex", flexDirection: "column", gap: "0px"}}>
-                                    <h2>{item.material}
-                                        <span className={"dimmed-text"}
-                                              style={{paddingLeft: "16px",}}>#{item.id}</span>
-                                    </h2>
-                                    <p>{item.vendor}</p>
+        <div className={"main flex flex-col items-center"}>
+            <h1>Štítky</h1>
+            <div className={"grid grid-cols-4 gap-4"}>
+                {filaments.map((item, key) => {
+                    return (
+                        <div className={"flex justify-between gap-8 w-max p-8 border"} key={key}>
+                            <div className={"flex flex-col gap-3"}>
+                                <div className={"flex gap-3 items-center"}>
+                                    <div
+                                        className={"w-12 h-12 rounded-full"}
+                                        style={{backgroundImage: `linear-gradient(135deg, ${item.color_hex}, ${item.color_second_hex || item.color_hex})`}}
+                                    />
+                                    <div>
+                                        <p className={"text-xl flex gap-4 justify-between leading-5"}>
+                                            <span className={"font-extrabold"}>{item.material}</span>
+                                            <span className={"font-extralight"}>#{item.id}</span>
+                                        </p>
+                                        <p className={""}>{item.vendor}</p>
+                                    </div>
                                 </div>
+                                <table className={"info-table"}>
+                                    <tbody>
+                                    <tr>
+                                        <td>Temp:</td>
+                                        <td>{item.temp_min} - {item.temp_max} °C</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Bed:</td>
+                                        <td>{item.temp_bed_min} {item.temp_bed_max && `- ${item.temp_bed_max}`}°C</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Price:</td>
+                                        <td>{(item.price || 0).toFixed(2)} €</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            <FilamentInfo item={item}/>
+                            <img
+                                className={"clickable-small w-32 h-32"} src={`http://${IP}:5000/api/images/qr/${item.id}.png/`}
+                                onClick={() => navigate(`/filament/${item.id}`)}
+                                alt=""
+                            />
                         </div>
-                        <img src={`http://${IP}:5000/api/images/qr/${item.id}.png`} alt=""
-                             style={{width: "12em", height: "12em", padding: "16px"}}/>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
         </div>
     );
 }
 
 export default Labels;
-
